2b3e57d91c1429d880797a6a04db40f909860f79
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-21 19:25:29 +0600

    Problem 1

--- assignment4.js
+++ assignment4.js
@@ -1,3 +1,8 @@
 function calculateTax(income, expenses) {
-          
+          if(income < 0 || expenses < 0 || expenses > income) {
+            return "Invalid Input";
+          }
+
+          return (income - expenses) * .20;
+
 }
