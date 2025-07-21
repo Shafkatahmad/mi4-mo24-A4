820bb187d2f04d43dbed3488cec93b706385f22f
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-21 22:13:25 +0600

    problem 3

--- assignment4.js
+++ assignment4.js
@@ -20,3 +20,14 @@ function sendNotification(email) {
 
 
 
+function checkDigitsInName(name) {
+          if(typeof(name) !== 'string'){
+            return "Invalid Input";
+          }
+          for(let i = 0; i < name.length; i++) {
+            if(isNaN(parseInt(name[i])) === false) {
+              return true;
+            }
+          }
+          return false;  
+}
